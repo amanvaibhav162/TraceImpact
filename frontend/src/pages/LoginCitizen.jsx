f92d23c495e6d5cfb12a7c29f1e95b5090df8852
amanvaibhav162 <amanvaibhav109@gmail.com>
2026-01-16 23:13:41 +0530

    login citizen page ui added

--- frontend/src/pages/LoginCitizen.jsx
+++ frontend/src/pages/LoginCitizen.jsx
@@ -3,25 +3,35 @@ import { Link } from "react-router-dom";
 
 function LoginCitizen() {
   return (
-    <div className="login-container">
-      <div className="container">
-        <div className="header">
-          <img src="/logo.png" alt="Logo" />
-        </div>
-        <h1>Login page for Citizen</h1>
-        <div className="form">
-          <p>
-            Username:
-            <input type="text" placeholder="Enter Username" />
+    <div className="login-page">
+      <div className="login-card split">
+        {/* LEFT – FORM */}
+        <div className="card-left">
+          <div className="header">
+            <img src="/logo.png" alt="Logo" />
+          </div>
+
+          <h1>Citizen Login</h1>
+
+          <input type="text" placeholder="Username" />
+          <input type="password" placeholder="Password" />
+          <input type="text" placeholder="Aadhaar Number" />
+
+          <p className="forgotpassword">
+            <Link to="/forgotpassword">Forgot Password?</Link>
           </p>
-          <p>
-            Password:
-            <input type="password" placeholder="Enter Password" />
-          </p>
+
+          <button className="login-button">Login</button>
+
+          <div className="noaccount">
+            Don’t have an account? <Link to="/signup">Sign Up</Link>
+          </div>
         </div>
-      </div>
-      <div className="image">
-        <img src="/Ourmission.png" alt="Login" />
+
+        {/* RIGHT – IMAGE */}
+        <div className="card-right">
+          <img src="/logincitizen.png" alt="Login visual" />
+        </div>
       </div>
     </div>
   );
